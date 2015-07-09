57761ad1c0f2ed606f8f87563bce6f0c7b502407
clarke <jordan.clarke@usask.ca>
2015-07-08 23:51:46 -0600

    Added in events for about and exit menus

--- software/gui/pad/frame.py
+++ software/gui/pad/frame.py
@@ -10,13 +10,24 @@ class Frame(wx.Frame):
         self.menu = wx.Menu()
         menuAbout = self.menu.Append(wx.ID_ABOUT,"&About",\
                 "The AutoFence: \nBy Clarke's Building")
+        self.Bind(wx.EVT_MENU, self.onAbout, menuAbout)
         self.menu.AppendSeparator()
         menuExit = self.menu.Append(wx.ID_EXIT,"E&xit",\
                 "Exit AutoFence")
+        self.Bind(wx.EVT_MENU, self.onExit, menuExit)
         menuBar = wx.MenuBar()
         menuBar.Append(self.menu, "&File")
         self.SetMenuBar(menuBar)
         self.Show(True)
+    ###################################################################
+    def onAbout(self, event):
+        aboutDialog = wx.MessageDialog(self,\
+                "The AutoFence: By Clarke's Building", "About", wx.OK)
+        aboutDialog.ShowModal()
+        aboutDialog.Destroy()
+    ###################################################################
+    def onExit(self, event):
+        self.Close(True)
 #######################################################################
 if __name__ == "__main__":
     app = wx.App(False)
